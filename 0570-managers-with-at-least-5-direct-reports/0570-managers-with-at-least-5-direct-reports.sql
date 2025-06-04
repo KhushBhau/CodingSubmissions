f0d95e52a67e6abb9f8bf642c8dd7e8c10e16ec0
KhushBhau <167280055+KhushBhau@users.noreply.github.com>
2025-06-04 20:05:37 +0530

    Time: 357 ms (52.15%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,7 +1,5 @@
-select e.name
-from Employee as e
-join employee as emp 
-on e.id=emp.managerId 
-GROUP BY e.Id ,e.name
-having Count(e.Id)>=5
-;
+select  e2.name as name from Employee e1 
+join Employee e2 
+on e1.managerId  = e2.id
+group by e1.managerId
+having count(distinct e1.id)>=5
